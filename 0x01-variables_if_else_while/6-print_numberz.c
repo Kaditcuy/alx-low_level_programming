ab986f195c88f5c8769f4c2edd50e627a0185c74
Kaditcuy <philipukchimaobi@gmail.com>
2022-07-01 07:11:44 -0700

    Added a few C scripts

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Write a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
+
 	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
