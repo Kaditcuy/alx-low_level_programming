67eef6425931f40d73432d18746ee65d51f612cf
Kaditcuy <philipukchimaobi@gmail.com>
2022-07-01 07:13:22 -0700

    Added a few C scripts

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - a program that prints the lowercase alphabet in reverse, followed by a new line.
+ * main - Entry point
  * Return Always 0 (Success)
  */
 int main(void)
 {
 	char n;
+
 	for (n = 'z'; n >= 'a'; n--)
 	{
 		putchar(n);
