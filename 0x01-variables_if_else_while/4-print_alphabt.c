4c394b169fbf29c4189f2d023a0fea7d2a57a209
Kaditcuy <philipukchimaobi@gmail.com>
2022-07-01 07:08:48 -0700

    Added a few C scripts

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,11 +2,12 @@
 
 /**
  * main - Entry point
- * return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char lowAlpha;
+
 	for (lowAlpha = 'a'; lowAlpha <= 'z'; lowAlpha++)
 	{
 		if (lowAlpha != 'q' && lowAlpha != 'e')
