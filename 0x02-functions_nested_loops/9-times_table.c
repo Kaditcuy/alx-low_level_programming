c1a1b0cd44a5568a1dded1aa97ce9e24ab0bb0fc
Kaditcuy <philipukchimaobi@gmail.com>
2022-07-07 14:52:00 -0700

    Hot fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,13 +14,26 @@ void times_table(void)
 	for (num1 = 0; num1 <= 9; num1++)
 	{
 		for (num2 = 0; num2 <= 9; num2++)
-		{		product = num1 * num2;
-				printf("%d", product);
-				putchar(44);
-				putchar(32);
-				putchar(32);
-		}
-		putchar('\n');
+		{
+			product = num1 * num2;
+
+			if (z == 0)
+				_putchar('0' + output);
+			else if (output >= 10)
+			{
+				_putchar(' ');
+				_putchar('0' + (output / 10));
+				_putchar('0' + (output % 10));
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + output);
+			}
+			if (z != 9)
+				_putchar(',');
+			}
+		_putchar('\n');
 	}
-return;
 }
