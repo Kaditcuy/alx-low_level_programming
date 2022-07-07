1375b877a07c3248358980ba16ed15a269234f39
Kaditcuy <philipukchimaobi@gmail.com>
2022-07-07 15:01:31 -0700

    Hot fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,15 +9,15 @@ void times_table(void)
 {
 	int num1;
 	int num2;
-	int product;
+	int output;
 
 	for (num1 = 0; num1 <= 9; num1++)
 	{
 		for (num2 = 0; num2 <= 9; num2++)
 		{
-			product = num1 * num2;
+			output = num1 * num2;
 
-			if (z == 0)
+			if (num2 == 0)
 				_putchar('0' + output);
 			else if (output >= 10)
 			{
@@ -31,7 +31,7 @@ void times_table(void)
 				_putchar(' ');
 				_putchar('0' + output);
 			}
-			if (z != 9)
+			if (num2 != 9)
 				_putchar(',');
 			}
 		_putchar('\n');
