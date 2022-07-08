8555dae9f07635a8e78476323cb538eeb13a07c3
Kaditcuy <philipukchimaobi@gmail.com>
2022-07-08 12:03:16 -0700

    Hot fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,11 +10,14 @@ int print_last_digit(int n)
 {
 	int x;
 
-	if (n < 0)
+	if (n >= 0)
 	{
-		n = -n;
+		x = n % 10;
 	}
-	x = n % 10;
-	_putchar(x + '0');
+	else
+	{
+		x = (n % 10) * -1;
+	}
+	_putchar('0' + x);
 	return (x);
 }
