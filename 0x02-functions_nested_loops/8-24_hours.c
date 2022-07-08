2d671dc03e859ecef02d95205028aac9525d01a1
Kaditcuy <philipukchimaobi@gmail.com>
2022-07-08 12:15:54 -0700

    Hot fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,29 +14,12 @@ void jack_bauer(void)
 	{
 		for (m = 0; m < 60; m++)
 		{
-			if (h < 10)
-			{
-				_putchar('0');
-				_putchar(h + '0'); /** 01,02,03,04,08,09*/
-			}
-			else if (h >= 10)
-			{
-				_putchar((h / 10) + '0');
-				_putchar((h % 10) + '0');
-			}
-			if (m < 10)
-			{
-				_putchar(':');
-				_putchar('0');
-				_putchar(m + '0');
-			}
-			else if (m >= 10)
-			{
-				_putchar(':');
-				_putchar((m / 10) + '0');
-				_putchar((m % 10) + '0');
-			}
-			putchar('\n');
+			_putchar('0' + (h / 10));
+			_putchar('0' + (h % 10));
+			_putchar(':');
+			_putchar('0' + (m / 10));
+			_putchar('0' + (m % 10));
+			_putchar('\n');
 		}
 	}
 }
