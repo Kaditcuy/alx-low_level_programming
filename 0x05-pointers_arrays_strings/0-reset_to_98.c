a5f8894d766c92dc8ec7a31fb3e5b9d91523fed9
Kaditcuy <philipukchimaobi@gmail.com>
2022-07-12 03:11:01 -0700

    Code to show pointer and dereferencing

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* reset_to_98 - Function to reset the value of a pointer variable
+* @*n: Takes a pointer to an int as an argument
+* Return: null Success
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
