a1068acd4762e099bec8fd2035130b87dfc0c86a
Kaditcuy <philipukchimaobi@gmail.com>
2022-07-23 10:50:08 -0700

    Function to return number of arguments passed into main

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - C program that prints the number of srguments passed into it 
+* main - C program that prints the number of srguments passed into it
 * @argc: Int argument of number of arguments passed to main
 * @argv: Pointer to char argument of array of strings
 * Return: O success
@@ -11,11 +11,10 @@ int main(int argc, char *argv[])
 	int i = 0;
 
 	(void)argv;
-	
-	while (i > argc-1)
+	while (i > argc - 1)
 	{
 		i++;
 	}
-	printf("%d\n",i);
+	printf("%d\n", i);
 	return (0);
 }
