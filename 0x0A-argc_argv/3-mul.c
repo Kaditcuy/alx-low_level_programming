5c5b4c1d67e76914f6c3b95e2cbf492d5c477c7d
Kaditcuy <philipukchimaobi@gmail.com>
2022-07-23 13:01:30 -0700

    hot fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,18 +12,18 @@ int main(int argc, char *argv[])
 	int mul = 0;
 	int num1 = 0, num2 = 0;
 
-	if (argc > 1)
-	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		mul = num1 * num2;
-
-		printf("%d\n", mul);
-	}
-	else if (argc - 1 < 2)
+	if (argc - 1 < 2)
 	{
 		printf("Error\n");
 		return (1);
+	}
+	else
+	{
+		num1 = atoi(argv[i]);
+		num2 = atoi(argv[i]);
+		mul = num1 * num2;
+
+		printf("%d\n", mul);
 	}
 	return (0);
 }
