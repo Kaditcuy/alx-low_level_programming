3844104f36af319f90c70d4064b74a940e3cb7a6
Kaditcuy <philipukchimaobi@gmail.com>
2022-08-01 14:11:05 -0700

    Cretaed a struct tpe dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - new type for dog
+* @name: char meember
+* @age: Float member
+* @owner: char argument
+*
+* Description: Structured type for Dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*DOG_H*/
