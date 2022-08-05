e2cbaec2a072a100046e67cf651b0869c0010819
Kaditcuy <philipukchimaobi@gmail.com>
2022-08-05 15:21:35 -0700

    Function to do basic operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -21,6 +21,13 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+
 	opS = *argv[2];
 
 	if (opS != '+' && opS != '-' && opS != '*' && opS != '/' && opS != '%')
