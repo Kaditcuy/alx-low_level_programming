ec5cb1042a35f2aae887b6e057a736b838a2d038
Kaditcuy <philipukchimaobi@gmail.com>
2022-09-30 04:13:21 -0700

    Added function to inser node at the begining of the list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,22 +9,23 @@
 */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node;
-
-	if (*head == NULL)
-		return (NULL);
-
-	new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
 	if (!new_node)
 		return (NULL);
 
 	new_node->n = n;
 	new_node->prev = NULL;
-	new_node->next = *head;
-	if (*head)
-		(*head)->prev = new_node;
-	*head = new_node;
+	new_node->next = NULL;
 
+	if (!*head)
+		*head = new_node;
+	else
+	{
+		new_node->next = *head;
+		new_node->prev = NULL;
+		(*head)->prev = new_node;
+		*head = new_node;
+	}
 	return (new_node);
 }
