13744624e1d90e051e41e5551ecd83b4dda126ee
Kaditcuy <philipukchimaobi@gmail.com>
2022-09-30 04:20:18 -0700

    rebase

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-* print_dlistint - prints all elements of a doubly linked list
+* print_dlistint_len - prints all elements of a doubly linked list
 * @h: node pointer holding address of first node in list
 *
 * Return: The number of nodes in the list
@@ -13,6 +13,9 @@ size_t dlistint_len(const dlistint_t *h)
 
 	count = 0;
 	ptr = h;
+
+	if (ptr == NULL)
+		return (0);
 
 	while (ptr != NULL)
 	{
