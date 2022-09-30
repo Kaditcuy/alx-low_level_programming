bda95b3ec854280790818d524fe78c1bce9e071d
Kaditcuy <philipukchimaobi@gmail.com>
2022-09-30 04:36:39 -0700

    Added function to insert node at the end of a list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -23,8 +23,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	else
 	{
 		tmp = *head;
-		while(tmp->next != NULL)
-				tmp = tmp->next;
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
 		new_node->next = NULL;
 		new_node->prev = tmp;
 		tmp->next = new_node;
